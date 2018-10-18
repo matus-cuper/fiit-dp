4c8c1a47f8e554657bd60c3c91e83f1de1a7604a
Matus Cuper <matus.cuper@gmail.com>
2018-10-18 18:22:56 +0200

    Refactor code and add removing uncomplete days

--- scripts/boilerplate.R
+++ scripts/boilerplate.R
@@ -13,16 +13,17 @@ WEEK <- 7
 
 loadDatasetFromDDC <- function(pathToFile) {
   train <- read.csv2(pathToFile, sep = ',')
-
   train$Values <- as.numeric(train$Values)
   train$Timestamp <- as.POSIXlt(train$Timestamp)
+
+  # For some reason, missing values are identified as 1, not NA
   train$Values[train$Values == 1] <- NA
 
-  tmp <- data.frame(train$Values, index(1:nrow(train)))
-  tmp <- na.approx(tmp, maxgap = 100)
-  tmp <- data.frame(tmp)
+  # Approximate missing values
+  approximation <- data.frame(train$Values, index(1:nrow(train)))
+  approximation <- data.frame(na.approx(approximation, maxgap = 100))
+  train$Values <- approximation$train.Values
 
-  train$Values <- tmp$train.Values
   colnames(train) <- c("timestamp", "load")
   return(train)
 }
@@ -51,7 +52,8 @@ loadDatasetFromSchool <- function(pathToFile) {
 }
 
 loadDataset <- function(pathToFile, groupBy = 1) {
-
+  pathToFile <- "~/r/fiit-dp/data/ddc/data/electricity/2.csv"
+  groupBy <- 2
   if (grepl("electricity", pathToFile)) {
     train <- loadDatasetFromDDC(pathToFile)
   } else if (grepl("summarized", pathToFile)) {
@@ -62,14 +64,27 @@ loadDataset <- function(pathToFile, groupBy = 1) {
     train <- loadDatasetFromSchool(pathToFile)
   }
 
-  if (!exists("train"))
+  if (!exists("train")) {
     return(NULL)
+  }
+
+  # First create date column
+  train <- train[!is.na(train$load), ]
+  train$date <- strftime(train$timestamp, "%Y-%m-%d")
+
+  # Find day period and remove uncomplete days
+  freq <- max(aggregate(load ~ date, data = train, FUN = length)$load)
+  agg <- aggregate(cbind(load) ~ date, data = train, length)
+  train <- train[train$date != agg[freq != agg$load,]$date, ]
+  #train[seconds_to_period(diff(as.POSIXct(train$date))) > period(seconds(86400)), ]
+
+  train$date <- NULL
 
   if (groupBy > 1) {
-    train$id <- floor(c((1:nrow(train))-1) / groupBy)
-    tmp <- aggregate(cbind(load) ~ id, data = train, sum)
-    timestamps <- train$timestamp[seq(0 + floor(groupBy/2), nrow(train), groupBy)]
-    train <- data.frame(timestamps, tmp$load)
+    train$id <- ceiling(c(1:nrow(train)) / groupBy)
+    agg <- aggregate(cbind(load) ~ id, data = train, sum)
+    timestamps <- train$timestamp[seq(1, nrow(train), groupBy)]
+    train <- data.frame(timestamps, agg$load)
   }
 
   colnames(train) <- c("timestamp", "load")
