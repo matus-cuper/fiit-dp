de1282153de8c4b73f779d040ff8827ef75006c8
Matus Cuper <matus.cuper@gmail.com>
2019-03-11 18:22:23 +0100

    Add some other filtering methods

--- scripts/filters.R
+++ scripts/filters.R
@@ -1,11 +1,27 @@
-filterWeekdays <- function(dataset, skipDays = c(6, 7)) {
+filterByDay <- function(dataset, selectDays) {
   if ("day" %in% names(dataset))
-    return(dataset[!dataset$day %in% skipDays, ])
-  return(dataset[!(as.POSIXlt(dataset$timestamp)$wday + 1) %in% skipDays, ])
+    return(dataset[dataset$day %in% selectDays, ])
+  return(dataset[(as.POSIXlt(dataset$timestamp)$wday + 1) %in% selectDays, ])
 }
 
-filterHolidays <- function(dataset, skipDays = 1) {
+filterByHoliday <- function(dataset, isHoliday) {
   if ("holiday" %in% names(dataset))
-    return(dataset[!dataset$holiday == skipDays, ])
-  return(dataset[!(as.POSIXlt(dataset$timestamp)$wday + 1) %in% skipDays, ])
+    return(dataset[dataset$holiday == isHoliday, ])
+  return(dataset[(as.POSIXlt(dataset$timestamp)$wday + 1) %in% isHoliday, ])
+}
+
+filterWeekdays <- function(dataset) {
+  return(filterByDay(dataset, 1:5))
+}
+
+filterWeekends <- function(dataset) {
+  return(filterByDay(dataset, 6:7))
+}
+
+filterWorkdays <- function(dataset) {
+  return(filterByHoliday(dataset, 0))
+}
+
+filterHolidays <- function(dataset) {
+  return(filterByHoliday(dataset, 1))
 }
