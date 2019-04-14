5ab0c3aa4d0093ec771242637a75f255b9933ef1
Matus Cuper <matus.cuper@gmail.com>
2019-04-14 13:06:24 +0200

    Change graphs to ggplot2

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -13,15 +13,33 @@ medians <- round(aggregate(. ~ group, groups, median)$val, 3)
 pips <- repr_pip(orig, times = length(orig) / 4 - 1)
 
 d <- data.frame(instances, samples, means, medians, pips)
-p <- plot_ly(d, x = ~instances, y = ~samples, name = 'Vzorkovanie dát', type = 'scatter', mode = 'lines') %>%
-  add_trace(y = ~means, name = 'Priemer', mode = 'lines') %>%
-  add_trace(y = ~medians, name = 'Medián', mode = 'lines') %>%
-  add_trace(y = ~pips, name = 'PIP', mode = 'lines') %>%
-  layout(xaxis = list(title = "Poradie meraní"), yaxis = list(title = "Hodnota"))
+p <- ggplot(d, aes(instances - 1)) +
+  geom_line(aes(y = samples, color = hue_pal()(5)[2]), size = 1) +
+  geom_line(aes(y = means, color = hue_pal()(5)[3]), size = 1) +
+  geom_line(aes(y = medians, color = hue_pal()(5)[4]), size = 1) +
+  geom_line(aes(y = pips, color = hue_pal()(5)[5]), size = 1) +
+  xlab("Poradie meraní") + ylab("Spotreba elektrickej energie v kW") +
+  scale_y_continuous(minor_breaks = seq(0, 5, 1), breaks = seq(0, 5, 1)) +
+  scale_x_continuous(minor_breaks = seq(0, 48, 6), breaks = seq(0, 48, 12)) +
+  theme(
+    legend.position = "none",
+    panel.grid.major = element_line(colour = "grey", size = 0.5),
+    panel.grid.minor = element_line(color = "grey"),
+    panel.background = element_rect(fill = "white", color = "black")
+  )
 print(p)
 
 inst <- c(1:length(orig))
 d <- data.frame(inst, orig)
-p <- plot_ly(d, x = ~inst, y = ~orig, name = 'Pôvodné dáta', type = 'scatter', mode = 'lines') %>%
-  layout(xaxis = list(title = "Poradie meraní"), yaxis = list(title = "Hodnota"), showlegend = TRUE)
+p <- ggplot(d, aes(inst - 1)) +
+  geom_line(aes(y = orig, color = hue_pal()(1)), size = 1) +
+  xlab("Poradie meraní") + ylab("Spotreba elektrickej energie v kW") +
+  scale_y_continuous(minor_breaks = seq(0, 5, 1), breaks = seq(0, 5, 1)) +
+  scale_x_continuous(minor_breaks = seq(0, 48, 6), breaks = seq(0, 48, 12)) +
+  theme(
+    legend.position = "none",
+    panel.grid.major = element_line(colour = "grey", size = 0.5),
+    panel.grid.minor = element_line(color = "grey"),
+    panel.background = element_rect(fill = "white", color = "black")
+  )
 print(p)
