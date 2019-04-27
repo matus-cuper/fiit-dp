4af73d53e46fe89cd93a0b875668568525a4674f
Matus Cuper <matus.cuper@gmail.com>
2019-04-27 15:10:45 +0200

    Finish SHESD chapter in solution design

---
+++ scripts/graphs-heatmap-days.R
@@ -0,0 +1,58 @@
+my.ireland <- data.frame(lapply(IRELAND[DATACOLUMNS], norm_z))
+my.ireland <- cbind(IRELAND[METADATACOLUMNS], my.ireland)
+
+SUSPICIOUS_HOLIDAYS <- scoreSuspiciousTS(CLUSTERING_HOLIDAYS)
+SUSPICIOUS_WORKDAYS <- scoreSuspiciousTS(CLUSTERING_WORKDAYS)
+
+my.selected <- "X1525.csv"
+my.df <- my.ireland[, c("timestamp", my.selected)]
+names(my.df) <- c("timestamp", "load")
+my.df$score <- 0
+
+for (my.col in 1:(WEEKS - 1)) {
+  my.s <- (my.col - 1) * WEEK * MPD + 1
+  my.e <- my.col * WEEK * MPD
+  
+  my.range.workdays <- as.numeric(rownames(filterWorkdays(filterWeekdays(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")]))))
+  my.range.holidays <- c(
+    as.numeric(rownames(filterHolidays(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")]))), 
+    as.numeric(rownames(filterWeekends(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")])))
+  )
+  
+  
+  if (my.col %in% c(1, WEEKS - 1)) {
+    if (my.col == 1) {
+      my.df$score[my.range.workdays] <- SUSPICIOUS_WORKDAYS[[my.selected]][1]
+      my.df$score[my.range.holidays] <- SUSPICIOUS_HOLIDAYS[[my.selected]][1]
+    }
+    else {
+      my.df$score[my.range.workdays] <- SUSPICIOUS_WORKDAYS[[my.selected]][my.col - 1]
+      my.df$score[my.range.holidays] <- SUSPICIOUS_HOLIDAYS[[my.selected]][my.col - 1]
+    }
+  }
+  else {
+    my.df$score[my.range.workdays] <- mean(SUSPICIOUS_WORKDAYS[[my.selected]][c(my.col - 1, my.col)])
+    my.df$score[my.range.holidays] <- mean(SUSPICIOUS_HOLIDAYS[[my.selected]][c(my.col - 1, my.col)])
+  }
+}
+
+
+my.df <- my.df[my.df$timestamp > "2009-07-31 23:59:59", ]
+
+my.df$weekday <- as.POSIXlt(my.df$timestamp)$wday
+my.df$weekdayFactor <- factor(lubridate::wday(my.df$timestamp, week_start = 1), levels = rev(1:7), labels = rev(c("Po", "Ut", "St", "Št", "Pi", "So", "Ne")), ordered = TRUE)
+my.df$monthFactor <- factor(month(my.df$timestamp), levels = as.character(1:12), labels = c("Jan", "Feb", "Mar", "Apr", "Máj", "Jún", "Júl", "Aug", "Sep", "Okt", "Nov", "Dec"), ordered = TRUE)
+my.df$yearMonth <- factor(as.yearmon(my.df$timestamp))
+my.df$week <- as.numeric(format(as.POSIXct(my.df$timestamp), "%W"))
+my.plotData <- ddply(my.df, .(yearMonth), transform, monthWeek = 1 + week - min(week))
+
+ggplot(my.plotData, aes(monthWeek, weekdayFactor, fill = my.plotData$score)) + 
+  geom_tile(colour = "white") + 
+  facet_grid(year(my.plotData$timestamp) ~ monthFactor) + 
+  scale_fill_gradient(low = "white", high = "red") + 
+  xlab("Týždeň mesiaca") + ylab("Dni v týždni") + labs(fill = "Skóre") +
+  theme(
+    panel.grid.major = element_line(colour = "grey", size = 0.5),
+    panel.grid.minor = element_line(color = "grey"),
+    panel.background = element_rect(fill = "white", color = "black")
+  )
---
+++ scripts/graphs-heatmap-hours.R
@@ -0,0 +1,58 @@
+my.ireland <- data.frame(lapply(IRELAND[DATACOLUMNS], norm_z))
+my.ireland <- cbind(IRELAND[METADATACOLUMNS], my.ireland)
+
+SUSPICIOUS_HOLIDAYS <- scoreSuspiciousTS(CLUSTERING_HOLIDAYS)
+SUSPICIOUS_WORKDAYS <- scoreSuspiciousTS(CLUSTERING_WORKDAYS)
+
+my.selected <- "X1525.csv"
+my.df <- my.ireland[, c("timestamp", my.selected)]
+names(my.df) <- c("timestamp", "load")
+my.df$score <- 0
+
+for (my.col in 1:(WEEKS - 1)) {
+  my.s <- (my.col - 1) * WEEK * MPD + 1
+  my.e <- my.col * WEEK * MPD
+  
+  my.range.workdays <- as.numeric(rownames(filterWorkdays(filterWeekdays(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")]))))
+  my.range.holidays <- c(
+    as.numeric(rownames(filterHolidays(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")]))), 
+    as.numeric(rownames(filterWeekends(my.ireland[my.s:my.e, c("timestamp", "day", "holiday")])))
+  )
+  
+  
+  if (my.col %in% c(1, WEEKS - 1)) {
+    if (my.col == 1) {
+      my.df$score[my.range.workdays] <- SUSPICIOUS_WORKDAYS[[my.selected]][1]
+      my.df$score[my.range.holidays] <- SUSPICIOUS_HOLIDAYS[[my.selected]][1]
+    }
+    else {
+      my.df$score[my.range.workdays] <- SUSPICIOUS_WORKDAYS[[my.selected]][my.col - 1]
+      my.df$score[my.range.holidays] <- SUSPICIOUS_HOLIDAYS[[my.selected]][my.col - 1]
+    }
+  }
+  else {
+    my.df$score[my.range.workdays] <- mean(SUSPICIOUS_WORKDAYS[[my.selected]][c(my.col - 1, my.col)])
+    my.df$score[my.range.holidays] <- mean(SUSPICIOUS_HOLIDAYS[[my.selected]][c(my.col - 1, my.col)])
+  }
+}
+
+my.merged <- merge(x = data.frame(timestamp  = my.df$timestamp), y = TWITTER_TS[[my.selected]]$anom$anoms, by = "timestamp", all.x = TRUE)
+my.merged$anoms[!is.na(my.merged$anoms)] <- 1
+my.merged$anoms[is.na(my.merged$anoms)] <- 0
+
+my.es <- c(es(my.merged$anoms)$fitted)
+my.es.rev <- rev(c(es(rev(my.merged$anoms))$fitted))
+
+my.df$smoothing <- (my.es + my.es.rev) / 2
+my.df$final <- my.df$score + my.df$smoothing
+my.df$date <- as.Date(my.df$timestamp, format = "%Y-%m-%d")
+my.df$hm <- format(as.POSIXct(my.df$timestamp), "%H:%M")
+my.lab <- with(my.df, paste(format(as.POSIXct(my.df$timestamp), "%H"), "00", sep = ":"))
+
+ggplot(data = my.df[1:2976, ], aes(x = date, y = hm, fill = final)) + geom_tile() +
+  xlab("Dátum") + ylab("Hodina") + labs(fill = "Skóre") +
+  scale_fill_gradient(low = "white", high="red") +
+  scale_y_discrete(breaks = my.lab) +
+  theme(
+    panel.background = element_rect(fill = "white", color = "black")
+  )
