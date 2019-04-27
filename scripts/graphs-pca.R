d3d4211eaaef1d1e5200f880f6371033b7161560
Matus Cuper <matus.cuper@gmail.com>
2019-04-27 11:44:03 +0200

    Fix figure caption

--- scripts/graphs-pca.R
+++ scripts/graphs-pca.R
@@ -3,8 +3,9 @@ df <- data.frame(
   y = rnorm(50, 2, 2)
 )
 p1 <- ggplot(df, aes(x = x, y = y)) + geom_point() +
-  xlab("dimenzia X") + ylab("dimenzia Y") +
+  xlab("dimenzia X") + ylab("dimenzia Y") + ggtitle("Pôvodný časový rad (2D)") +
   theme(
+    plot.title = element_text(hjust = 0.5),
     panel.grid.major = element_line(colour = "grey"),
     panel.grid.minor = element_line(color = "grey"),
     panel.background = element_rect(fill = "white", color = "black")
@@ -12,8 +13,9 @@ p1 <- ggplot(df, aes(x = x, y = y)) + geom_point() +
 
 df.pca <- prcomp(df)
 p2 <- ggplot(as.data.frame(df.pca$x), aes(x = df.pca$x[, 1], y = c(0))) + geom_point() +
-  xlab("PCA1") +
+  xlab("PCA1") + ggtitle("Redukovaný časový rad (1D)") +
   theme(
+    plot.title = element_text(hjust = 0.5),
     axis.title.y = element_blank(),
     axis.text.y = element_blank(),
     panel.grid.major = element_line(colour = "grey"),
